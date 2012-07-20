3ebe32394807aac02681f75f97f53f2546d0d402
Antoine Beaupré <anarcat@koumbit.org>
2012-07-19 21:36:49 -0400

    rewrite tests to use the temporary keyring class

--- tests/test_gpg.py
+++ tests/test_gpg.py
@@ -7,11 +7,19 @@ from monkeysign import Gpg, GpgTemp
 
 class TestGpg(unittest.TestCase):
     def setUp(self):
-        self.gpg = Gpg()
+        self.gpg = Gpg('/tmp/gpg-home')
 
     def test_env(self):
-        self.gpg = Gpg('/tmp/gpg-home')
         self.assertEqual(os.environ['GPG_HOME'], '/tmp/gpg-home')
+
+class TestGpgTemp(unittest.TestCase):
+    def setUp(self):
+        # we test using the temporary keyring because it's too dangerous otherwise
+        if 'GPG_HOME' in os.environ: del os.environ['GPG_HOME']
+        self.gpg = GpgTemp()
+
+    def test_env(self):
+        self.assertTrue(os.path.exists(os.environ['GPG_HOME']))
 
     def test_command(self):
         c = self.gpg.build_command([])
@@ -22,13 +30,8 @@ class TestGpg(unittest.TestCase):
     def test_version(self):
         self.assertTrue(self.gpg.version())
 
-class TestGpgTemp(unittest.TestCase):
-
-    def setUp(self):
-        self.gpg = GpgTemp()
-        
-    def test_cleanup(self):
-        self.assertTrue(os.path.exists(os.environ['GPG_HOME']))
+    def tearDown(self):
+        del self.gpg
 
 if __name__ == '__main__':
     unittest.main()
