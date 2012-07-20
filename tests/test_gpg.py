324b35b81dce7b44ff34da2a90ba5df8d7fbee14
Antoine Beaupré <anarcat@koumbit.org>
2012-07-20 00:24:21 -0400

    simplify and cleanup commandline processing

--- tests/test_gpg.py
+++ tests/test_gpg.py
@@ -18,13 +18,25 @@ class TestGpgTemp(unittest.TestCase):
         if 'GPG_HOME' in os.environ: del os.environ['GPG_HOME']
         self.gpg = GpgTemp()
 
+    def test_set_option(self):
+        self.gpg.set_option('armor')
+        self.assertIn('armor', self.gpg.options)
+        self.gpg.set_option('keyserver', 'foo.example.com')
+        self.assertDictContainsSubset({'keyserver': 'foo.example.com'}, self.gpg.options)
+
+    def test_build_command(self):
+        # reset options to a known setting
+        options = { 'status-fd': 1, 'command-fd': 0, 'no-tty': None, 'use-agent': None }
+        self.gpg.options = options
+        self.assertEqual(self.gpg.build_command(['list-keys', 'foo']), ['gpg', '--command-fd', '0', '--no-tty', '--status-fd', '1', '--use-agent', '--list-keys', 'foo' ])
+
     def test_env(self):
         self.assertTrue(os.path.exists(os.environ['GPG_HOME']))
 
     def test_command(self):
         c = self.gpg.build_command([])
         c.append('--version')
-        c2 = self.gpg.build_command(['--version'])
+        c2 = self.gpg.build_command(['version'])
         self.assertEqual(c, c2)
 
     def test_version(self):
@@ -35,7 +47,7 @@ class TestGpgTemp(unittest.TestCase):
 
     def test_export(self):
         k1 = open(os.path.dirname(__file__) + '/7B75921E.asc').read()
-        self.gpg.add_option(['--armor'])
+        self.gpg.set_option('armor')
         k2 = self.gpg.export_data('7B75921E')
         self.assertEqual(k1,k2)
 
