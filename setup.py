516fa8e80fcafbb36d4db68af1db7525e56f5954
Antoine Beaupré <anarcat@koumbit.org>
2012-07-21 12:49:10 -0400

    copy README to the long description

--- setup.py
+++ setup.py
@@ -6,7 +6,18 @@ from gpg import __version__ as version
 
 setup(name = 'monkeysign',
     description='OpenPGP key exchange for humans',
-    long_description='This tool makes it easier to sign and exchange OpenPGP keys.',
+    long_description="""
+monkeysign is a tool to overhaul the OpenPGP keysigning experience and
+bring it closer to something that most primates can understand.
+
+The project makes use of cheap digital cameras and the type of bar
+code known as a QRcode to provide a human-friendly yet still-secure
+keysigning experience.
+
+No more reciting tedious strings of hexadecimal characters.  And, you
+can build a little rogue's gallery of the people that you have met and
+exchanged keys with!
+""",
     version=version,
     author='Antoine Beaupré',
     author_email='anarcat@debian.org',
