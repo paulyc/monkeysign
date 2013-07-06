6779a17da7bbb8548065bb1f8a7a534823556d81
Antoine Beaupré <anarcat@koumbit.org>
2013-07-06 01:06:05 -0400

    include proper copyright headers everywhere

--- monkeysign/gpg.py
+++ monkeysign/gpg.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-#    Copyright (C) 2012 Antoine Beaupré <anarcat@orangeseeds.org>
+#    Copyright (C) 2012-2013 Antoine Beaupré <anarcat@orangeseeds.org>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
