a9d81360ad59ef5cb671c1dba59983b02bd1c301
Antoine Beaupré <anarcat@koumbit.org>
2013-07-17 22:55:19 -0400

    prepare 0.8

--- monkeysign/__init__.py
+++ monkeysign/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version_info__ = ('0', '7')
+__version_info__ = ('0', '8')
 __version__ = '.'.join(__version_info__)
 __copyright__ = """Copyright (C) 2010-2013  Antoine Beaupré, Jerome Charaoui
 This program comes with ABSOLUTELY NO WARRANTY.
