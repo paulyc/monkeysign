978222f1fe2e01cbe65c50fa25cdf9b546f6ea14
Antoine Beaupré <anarcat@koumbit.org>
2013-08-05 13:22:05 +0200

    fix indentation in uid selection

--- monkeysign/cli.py
+++ monkeysign/cli.py
@@ -82,14 +82,14 @@ script assumes you have gpg-agent configure to prompt for passwords."""
             for uid in key.uidslist:
                 allowed_uids.append(uid.uid)
 
-                prompt += ' (1-%d or full UID, control-c to abort): ' % len(allowed_uids)
+            prompt += ' (1-%d or full UID, control-c to abort): ' % len(allowed_uids)
 
+            pattern = raw_input(prompt)
+            while not (pattern in allowed_uids or (pattern.isdigit() and int(pattern)-1 in range(0,len(allowed_uids)))):
+                print "invalid uid"
                 pattern = raw_input(prompt)
-                while not (pattern in allowed_uids or (pattern.isdigit() and int(pattern)-1 in range(0,len(allowed_uids)))):
-                    print "invalid uid"
-                    pattern = raw_input(prompt)
-                if pattern.isdigit():
-                    pattern = allowed_uids[int(pattern)-1]
+            if pattern.isdigit():
+                pattern = allowed_uids[int(pattern)-1]
             return pattern
         except KeyboardInterrupt:
             return False
