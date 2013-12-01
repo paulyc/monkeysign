4d170c6ee21d63caaf5b5edfbb359ece4d87238b
Antoine Beaupré <anarcat@koumbit.org>
2013-11-30 23:27:34 -0500

    make this more generic, add action handlers

--- doc/ui-mockups/test-glade.py
+++ doc/ui-mockups/test-glade.py
@@ -8,19 +8,39 @@ except:
     print "You need to install pyGTK or GTKv2 ",
     sys.exit(1)
 
+class Handler:
+    """
+    This handler gets called by Gtk.Builder() based on the signals
+    defined in the XML UI.
+    """
+    def quit(self, *args):
+        Gtk.main_quit(*args)
+
+    def button(self, button):
+        print "Hello World!"
+        print "I am a button!" + repr(button)
 
 class monkeysharegui:
+    """
+    This class builds the UI from the glade files and shows all
+    windows, but doesn't start the GTK threads.
+    """
     def __init__(self):
         """
         """
         builder = Gtk.Builder()
-        builder.add_from_file("share-ui.glade")
-        window = builder.get_object("monkeyshare")
+        builder.add_from_file(sys.argv[1])
+        builder.connect_signals(Handler())
+        window = builder.get_object(sys.argv[2])
         window.show_all()
         return
 
-    def button1_clicked(self,widget):
-        print "button clicked"
-
-monkeysharegui()
+try:
+    monkeysharegui()
+except IndexError:
+    print "pass the name of the glade file to load and the name of the main window"
+    sys.exit(1)
+except AttributeError:
+    print "wrong window name"
+    sys.exit(1)
 Gtk.main()
