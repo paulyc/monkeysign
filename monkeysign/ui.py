paulyc/monkeysign

commit 1dbc8ce4faf3591d7885a9c86fb09d4ec54cd5b4

handle errors better when connecting to SMTP servers (Closes: #756540)

monkeysign/ui.py
@@ -338,8 +338,16 @@ Sign all identities? [y/N] \
 expects an EmailFactory email, but will not mail if nomail is set"""
             if self.options.smtpserver is not None and not self.options.nomail:
                 if self.options.dryrun: return True
-                server = smtplib.SMTP(self.options.smtpserver)
+                server = smtplib.SMTP()
                 server.set_debuglevel(self.options.debug)
+                # to be nicer to users, we could catch socket.error exceptions from
+                # server.connect() here and display a meaningful message to stderr.
+                try:
+                    (code, msg) = server.connect(self.options.smtpserver)
+                except (socket.error, socket.timeout) as e:
+                    self.abort(_('Error connecting to SMTP server: %s') % e)
+                if code != 220:
+                    self.abort(_('Unexpected SMTP server '%s' error code: %s (%s)') % (self.options.smtpserver, code, msg))
                 try:
                     server.starttls()
                 except SMTPException:

commit 7c5568fdee3f46177f913a99c98f245d07034837

smtp reports a stack trace
commit 1dbc8ce4faf3591d7885a9c86fb09d4ec54cd5b4 broke smtp: the msg
variable to the function gets overridden by the return value of the smtp
connection.

monkeysign/ui.py
@@ -343,11 +343,11 @@ expects an EmailFactory email, but will not mail if nomail is set"""
                 # to be nicer to users, we could catch socket.error exceptions from
                 # server.connect() here and display a meaningful message to stderr.
                 try:
-                    (code, msg) = server.connect(self.options.smtpserver)
+                    (code, srvmsg) = server.connect(self.options.smtpserver)
                 except (socket.error, socket.timeout) as e:
                     self.abort(_('Error connecting to SMTP server %s: %s') % (self.options.smtpserver, e))
                 if code != 220:
-                    self.abort(_('Unexpected SMTP server error while talking to %s, code: %s (%s)') % (self.options.smtpserver, code, msg))
+                    self.abort(_('Unexpected SMTP server error while talking to %s, code: %s (%s)') % (self.options.smtpserver, code, srvmsg))
                 try:
                     server.starttls()
                 except SMTPException: